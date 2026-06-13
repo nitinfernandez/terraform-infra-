b4a800d14c940c8e792788b624cf876b9ff47a2f
Nitin Fernandez <nitin.fernandez10@gmail.com>
2026-06-13 05:09:46 +0100

    Terraform Project

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,11 +9,13 @@ resource "aws_vpc" "this" {
 
 
 resource "aws_subnet" "this" {
+  count = length(var.subnet_cidrs)
+
   vpc_id     = aws_vpc.this.id
-  cidr_block = var.subnet_cidr
+  cidr_block = var.subnet_cidrs[count.index]
 
   tags = {
-    Name = "${var.name}-subnet"
+    Name = "${var.name}-subnet-${count.index + 1}"
   }
 }
 
@@ -33,6 +35,8 @@ resource "aws_route" "default" {
 }
 
 resource "aws_route_table_association" "subnet" {
-  subnet_id      = aws_subnet.this.id
+  count = length(var.subnet_cidrs)
+
+  subnet_id      = aws_subnet.this[count.index].id
   route_table_id = aws_route_table.public.id
 }
